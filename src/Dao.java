6ea767505715b50ddb1e0faa6115496c73e82fe9
chacha86 <taejincha0619@gmail.com>
2021-05-30 17:02:54 +0900

    수정/삭제/검색

--- src/Dao.java
+++ src/Dao.java
@@ -42,4 +42,54 @@ public class Dao {
 		
 		return addrList;
 	}
+	
+	public void insertAddr(Addr addr) throws ClassNotFoundException, SQLException {
+		Connection conn = getConnection();
+		Statement stmt = conn.createStatement();
+		String sql = "INSERT INTO addr SET `name`='" + addr.getName() + "', `addr`='" + addr.getAddr() + "', `phone`='" + addr.getPhone() + "'";
+		stmt.executeUpdate(sql);
+	}
+
+	public void updateAddr(Addr addr) throws ClassNotFoundException, SQLException {
+		Connection conn = getConnection();
+		Statement stmt = conn.createStatement();
+		String sql = "UPDATE addr SET `name`='"+ addr.getName() + "', `addr`='" + addr.getAddr() + "', `phone`='" + addr.getPhone() + "' WHERE id = " + addr.getId();
+		stmt.executeUpdate(sql);
+	}
+	
+	public void deleteAddr(int id) throws ClassNotFoundException, SQLException {
+		Connection conn = getConnection();
+		Statement stmt = conn.createStatement();
+		String sql = "DELETE FROM addr WHERE id = " + id;
+		stmt.executeUpdate(sql);
+	}
+
+	public ArrayList<Addr> getSearchedAddrByName(String keyword) throws ClassNotFoundException, SQLException {
+
+		Connection conn = getConnection();
+		Statement stmt = conn.createStatement();
+		String sql = "SELECT * FROM addr WHERE name LIKE '%" + keyword + "%'";
+		
+		System.out.println(sql);
+		ResultSet rs = stmt.executeQuery(sql);
+		
+		ArrayList<Addr> addrList = new ArrayList<>();
+		
+		while(rs.next()) {
+			int id = rs.getInt("id");
+			String name = rs.getString("name");
+			String addr = rs.getString("addr");
+			String phone = rs.getString("phone");
+			
+			Addr a = new Addr();
+			a.setId(id);
+			a.setName(name);
+			a.setAddr(addr);
+			a.setPhone(phone);
+			
+			addrList.add(a);
+		}
+		
+		return addrList;
+	}
 }
--- src/Exam.java
+++ src/Exam.java
@@ -26,15 +26,48 @@ public class Exam {
 				System.out.println("전화번호를 입력해주세요 : ");
 				String phone = scan.nextLine();
 				
-//				Statement stmt = conn.createStatement();
-//				String sql = "INSERT INTO addr SET `name`='" + name + "', `addr`='" + addr + "', `phone`='" + phone + "'";
-//				System.out.println(sql);
-//				stmt.executeUpdate(sql);
+				Addr a = new Addr();
+				a.setAddr(addr);
+				a.setName(name);
+				a.setPhone(phone);
+				
+				dao.insertAddr(a);
 				
 			} else if(cmd.equals("list")) {
 				ArrayList<Addr> list = dao.getAddrList();
 				view.printAddrList(list);
-				//wv.printAddrList(list);
+
+			} else if(cmd.equals("update")) {
+				System.out.println("수정할 주소록 번호를 입력해주세요 :");
+				int id = Integer.parseInt(scan.nextLine());
+				System.out.println("이름을 입력해주세요 : ");
+				String name = scan.nextLine();
+				System.out.println("주소를 입력해주세요 : ");
+				String addr = scan.nextLine();
+				System.out.println("전화번호를 입력해주세요 : ");
+				String phone = scan.nextLine();
+				
+				Addr a = new Addr();
+				a.setId(id);
+				a.setAddr(addr);
+				a.setName(name);
+				a.setPhone(phone);
+				
+				dao.updateAddr(a);
+				
+			} else if(cmd.equals("delete")) {
+				
+				System.out.println("삭제할 주소록 번호를 입력해주세요 : ");
+				int id = Integer.parseInt(scan.nextLine());
+				dao.deleteAddr(id);
+				
+			} else if(cmd.equals("search")) {
+				
+				System.out.println("검색할 대상의 이름을 입력해주세요 : ");
+				String name = scan.nextLine();
+				ArrayList<Addr> list = dao.getSearchedAddrByName(name);
+				view.printAddrList(list);
+				
 			}
 		}
 	}
